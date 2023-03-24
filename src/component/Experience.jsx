8fe092f91797bf43483074c2b5fdf38de3b00479
Kepler Fontenele <keplerf@criticalmass.com>
2023-03-24 12:10:44 -0700

    init

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,8 +1,7 @@
 import React, { useRef } from "react";
-import { extend, useFrame, useThree } from "@react-three/fiber";
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { useFrame, useThree } from "@react-three/fiber";
 import CustomObject from "./CustomObject";
-extend({ OrbitControls });
+import { OrbitControls, TransformControls } from "@react-three/drei";
 
 const Experience = () => {
   const boxRef = useRef();
@@ -11,12 +10,17 @@ const Experience = () => {
 
   useFrame((state, delta) => {
     boxRef.current.rotation.y += delta;
+    const angle = state.clock.elapsedTime * 0.3;
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+    // state.camera.lookAt(0, 0, 0);
+
     // groupRef.current.rotation.y += delta * 0.45;
   });
 
   return (
     <>
-      <orbitControls args={[camera, gl.domElement]} />
+      <OrbitControls makeDefault />
       <directionalLight position={[1, 2, 3]} intensity={1.4} />
       <ambientLight intensity={0.2} />
       <group ref={groupRef}>
@@ -27,7 +31,7 @@ const Experience = () => {
           scale={1.5}
         >
           <boxGeometry />
-          <meshStandardMaterial color="red" />
+          <meshStandardMaterial color={"#ff0000"} />
         </mesh>
 
         <mesh position={[-2, 0, 0]}>
@@ -35,6 +39,8 @@ const Experience = () => {
           <meshStandardMaterial color="cyan" />
         </mesh>
       </group>
+      <TransformControls object={groupRef} />
+
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={18}>
         <planeGeometry />
         <meshStandardMaterial color="gray" />
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,28 @@
 import "./style.css";
 import ReactDOM from "react-dom/client";
 import React from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame } from "@react-three/fiber";
 import Experience from "./component/Experience";
+import * as THREE from "three";
 
 const root = ReactDOM.createRoot(document.querySelector("#root"));
 
+const cameraSetting = {
+  fov: 45,
+  near: 1,
+  far: 20,
+  position: [3, 1, 5],
+};
+
 root.render(
   <Canvas
-    camera={{
-      fov: 45,
-      near: 1,
-      far: 20,
-      position: [3, 4, 6],
-    }}
+    // dpr={[1, 2]} default
+    // gl={{
+    //     antialias: true,
+    //     toneMapping: THREE.CineonToneMapping
+    // }}
+    // flat
+    camera={cameraSetting}
   >
     <Experience />
   </Canvas>
