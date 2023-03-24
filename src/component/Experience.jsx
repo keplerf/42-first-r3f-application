30f61ee5158f5a89ef3a351d4ef49a95a5aa7369
Kepler Fontenele <keplerf@criticalmass.com>
2023-03-24 12:32:17 -0700

    init

--- src/component/Experience.jsx
+++ src/component/Experience.jsx
@@ -1,7 +1,12 @@
 import React, { useRef } from "react";
 import { useFrame, useThree } from "@react-three/fiber";
 import CustomObject from "./CustomObject";
-import { OrbitControls, TransformControls } from "@react-three/drei";
+import {
+  Html,
+  OrbitControls,
+  TransformControls,
+  PivotControls,
+} from "@react-three/drei";
 
 const Experience = () => {
   const boxRef = useRef();
@@ -24,6 +29,7 @@ const Experience = () => {
       <directionalLight position={[1, 2, 3]} intensity={1.4} />
       <ambientLight intensity={0.2} />
       <group ref={groupRef}>
+        {/* <PivotControls anchor={[0, 0, 0]} scale={3.0} depthTest={false}> */}
         <mesh
           ref={boxRef}
           position={[2, 0, 0]}
@@ -33,13 +39,14 @@ const Experience = () => {
           <boxGeometry />
           <meshStandardMaterial color={"#ff0000"} />
         </mesh>
-
+        {/* </PivotControls> */}
         <mesh position={[-2, 0, 0]}>
           <sphereGeometry />
           <meshStandardMaterial color="cyan" />
+          <Html>dsd</Html>
         </mesh>
       </group>
-      <TransformControls object={groupRef} />
+      <TransformControls object={groupRef} mode="translate" />
 
       <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={18}>
         <planeGeometry />
